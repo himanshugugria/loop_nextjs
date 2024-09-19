5b197115d6b035d3ada7c2f1c5fc5f2f5510a26f
himanshugugria <himanshugugria@gmail.com>
2024-09-19 19:49:24 +0530

    auth_using clerk

---
+++ app/(routes)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+function dashboard() {
+  return (
+    <div>
+      <h2>dashboard</h2>
+      
+    </div>
+  )
+}
+
+export default dashboard
